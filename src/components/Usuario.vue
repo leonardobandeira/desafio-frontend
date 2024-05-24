3849cd31f652ca39f96bdd852b53f983b5ebfcb1
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-24 02:44:57 -0300

    feat: Ajusta componente de Usuário logado

--- src/components/Usuario.vue
+++ src/components/Usuario.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="usuario">
-    <i class="fa-solid fa-user"></i> <span>{{ usuario }}</span>
+    <i class="far fa-user-circle icone__user"></i> <span>{{ usuario }}</span><i class="fa-solid fa-right-from-bracket icone__exit"></i>
   </div>
 </template>
 
@@ -21,5 +21,15 @@ export default {
   display: flex;
   align-items: center;
   padding: 0 10px;
+}
+
+.icone__user{
+  font-size: 2em;
+  padding-right: 0.2em;
+}
+
+.icone__exit{
+  font-size: 1em;
+  padding-left: 1em;
 }
 </style>
