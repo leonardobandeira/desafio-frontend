b0418541647da9e621cacd9e9ec683881a58993f
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-24 02:07:36 -0300

    feat: Cria elementos para Logo e Usuário logado

---
+++ src/components/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="logo">
+    <img :src="imagem" :alt="descricao" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo",
+  props: {
+    imagem: {
+      type: String,
+      required: true,
+    },
+    descricao: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+img {
+  width: 15em;
+  height: auto;
+  display: block;
+}
+</style>
---
+++ src/components/Usuario.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="usuario">
+    <i class="fa-solid fa-user"></i> <span>{{ usuario }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Usuario",
+  props: {
+    usuario: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.usuario {
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+}
+</style>
