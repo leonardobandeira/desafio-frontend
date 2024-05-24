646eeba341379e357e5d98e3348a12c80a22e587
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-24 03:44:56 -0300

    feat: Adiciona seção de filtros inicial ao projeto

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="content">
-    <div class="row__titulo">
+    <div class="row content__titulo">
       <span class="titulo">BTC Carteiras</span>
       <button class="btn__add__carteira">Adicionar Carteira</button>
+    </div>
+
+    <div class="row content__filter">
+      <FormInput
+        type="text"
+        name="nome"
+        id="nome"
+        placeholder="Informe o nome do cliente"
+      />
+      <FormInput
+        type="text"
+        name="sobrenome"
+        id="sobrenome"
+        placeholder="Informe o sobrenome do cliente"
+      />
+      <FormInput
+        type="text"
+        name="email"
+        id="email"
+        placeholder="Informe o email do cliente"
+      />
+      <button class="btn__buscar">
+        <i class="fa-solid fa-magnifying-glass icon__buscar"></i>Buscar
+      </button>
+    </div>
+
+    <div class="row content__tabela">
+      slkanalcasslkanalcasslkanalcasslkanalcassl
+      kanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkan
+      alcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcas
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import FormInput from "./../components/FormInput";
+export default {
+  name: "Content",
+  components: {
+    FormInput,
+  },
+};
 </script>
 
 <style>
@@ -18,7 +54,7 @@ export default {};
   background-color: #f8f8f8;
 }
 
-.row__titulo {
+.content__titulo {
   display: flex;
   justify-content: space-between;
 }
@@ -37,5 +73,42 @@ export default {};
   padding: 0.4em 1em;
   border: #007bff;
   border-radius: 4px;
+}
+
+.content__filter {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  background: #fff;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 4em;
+  margin: 2em 0 2em 0;
+  border-radius: 0.2em;
+}
+
+.btn__buscar {
+  background: #fff;
+  border: solid 0.12em #007bff;
+  font-size: 1em;
+  width: 7em;
+  height: 2.4em;
+  border-radius: 6px;
+  color: #007bff;
+}
+
+.icon__buscar {
+  padding-right: 1em;
+}
+
+.content__tabela {
+  background: #fff;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  justify-content: left;
+  padding: 1.5em 0.5em;
+  background: red;
 }
 </style>
