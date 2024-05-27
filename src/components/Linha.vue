7a7a627b9038004b029352c0e583da55a21d7ac0
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-27 03:24:39 -0300

    feat: Cria component Linha

---
+++ src/components/Linha.vue
@@ -0,0 +1,19 @@
+<template>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: "Linha",
+};
+</script>
+
+<style>
+hr {
+  width: 100%;
+  height: 0.01em;
+  border: 0;
+  padding: 0;
+  border-top: 1px solid rgb(221, 220, 220);
+}
+</style>
