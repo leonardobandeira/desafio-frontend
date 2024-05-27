df5723d223346445c973fabcd9b78ab0a3d8b001
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-27 01:22:36 -0300

    feat: Cria elemento Botão estilizado

---
+++ src/components/form/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="btn__add__carteira" @click="acao">{{ texto }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    tipo: {
+      type: String,
+      required: true,
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+    acao: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.btn__add__carteira {
+  color: #fff;
+  font-weight: 400;
+  font-size: 1em;
+  background-color: #007bff;
+  padding: 0.4em 1em;
+  border: #007bff;
+  border-radius: 4px;
+}
+</style>
