c3438ef41179163d853c8fe41d1e5fb5604b63bb
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-27 01:29:01 -0300

    feat: Cria elemento de title para o projeto

---
+++ src/components/form/Title.vue
@@ -0,0 +1,33 @@
+<template>
+  <span :class="tipo">{{ texto }}</span>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    tipo: {
+      type: String,
+      required: true,
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.primary {
+  color: #000;
+  font-size: 1.5em;
+  font-weight: 700;
+}
+
+.secondary {
+  color: rgb(146, 155, 155);
+  font-size: 1em;
+  font-weight: 700;
+}
+</style>
--- src/components/portfolio/HeaderPage.vue
+++ src/components/portfolio/HeaderPage.vue
@@ -1,33 +1,25 @@
 <template>
   <div class="row content__titulo">
-    <span class="titulo">BTC Carteiras</span>
-    <button class="btn__add__carteira">Adicionar Carteira</button>
+    <Title tipo="primary" texto="BTC Carteiras"/>
+    <Button tipo="add" texto="Adicionar Carteira" acao="add" />
   </div>
 </template>
 
 <script>
-export default {};
+import Title from "./../form/Title";
+import Button from "./../form/Button";
+export default {
+  name: "HeaderPage",
+  components: {
+    Title,
+    Button,
+  },
+};
 </script>
 
 <style>
 .content__titulo {
   display: flex;
   justify-content: space-between;
-}
-
-.titulo {
-  color: #000;
-  font-size: 1.5em;
-  font-weight: 700;
-}
-
-.btn__add__carteira {
-  color: #fff;
-  font-weight: 400;
-  font-size: 1em;
-  background-color: #007bff;
-  padding: 0.4em 1em;
-  border: #007bff;
-  border-radius: 4px;
 }
 </style>
