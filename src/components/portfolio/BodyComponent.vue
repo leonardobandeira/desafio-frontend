3657d095c83e3d2ac9ecb6ac4b1633eb1f89e60f
Leonardo Lucena <111189298+leonardo-lucena@users.noreply.github.com>
2024-05-27 03:25:03 -0300

    feat: Cria elemento BodyComponent e adiciona ao projeto

---
+++ src/components/portfolio/BodyComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row content__tabela">
+    <HeaderComponent>
+      <template #left>
+        <Title tipo="secondary" texto="Carteiras" />
+      </template>
+      <template #right>
+        <Button tipo="buscar" texto="Exportar CSV" acao="exportar" />
+      </template>
+    </HeaderComponent>
+    <br />
+    <Table />
+    <Linha />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./HeaderComponent";
+
+import Title from "./../form/Title";
+import Button from "./../form/Button";
+import Table from "./../form/Table";
+import Linha from "./../Linha";
+export default {
+  name: "BodyComponent",
+  components: {
+    HeaderComponent,
+
+    Title,
+    Button,
+
+    Table,
+
+    Linha,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -10,17 +10,14 @@
     </HeaderComponent>
 
     <FilterComponent />
-    <div class="row content__tabela">
-      slkanalcasslkanalcasslkanalcasslkanalcassl
-      kanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkan
-      alcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcasslkanalcas
-    </div>
+    <BodyComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./../components/portfolio/HeaderComponent";
 import FilterComponent from "./../components/portfolio/FilterComponent";
+import BodyComponent from "./../components/portfolio/BodyComponent";
 
 import Title from "./../components/form/Title";
 import Button from "./../components/form/Button";
@@ -29,6 +26,8 @@ export default {
   components: {
     HeaderComponent,
     FilterComponent,
+    BodyComponent,
+
     Title,
     Button,
   },
@@ -40,14 +39,5 @@ export default {
   grid-area: content;
   padding: 3em 10em 0 10em;
   background-color: #f8f8f8;
-}
-
-.content__tabela {
-  background: #fff;
-  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.1);
-  border-radius: 4px;
-  justify-content: left;
-  padding: 1.5em 0.5em;
-  background: red;
 }
 </style>
